b97085c313848075712f8f8db05944b077112c1c
ClementMs <clement.msika@gmail.com>
2025-01-24 08:12:00 +0100

    updated app

---
+++ streamlit_app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from langchain_openai.chat_models import ChatOpenAI
+
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
+
+# Access the environmental variables
+openai_api_key = os.getenv('openai_api_key')
+claude_api_key = os.getenv('claude_api_key')
+
+st.title("🦜🔗 Quickstart App")
+
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
+
+def generate_response(input_text):
+    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
+    st.info(model.invoke(input_text))
+
+
+with st.form("my_form"):
+    text = st.text_area(
+        "Enter text:",
+        "What are the three key pieces of advice for learning how to code?",
+    )
+    submitted = st.form_submit_button("Submit")
+    if not openai_api_key.startswith("sk-"):
+        st.warning("Please enter your OpenAI API key!", icon="⚠")
+    if submitted and openai_api_key.startswith("sk-"):
+        generate_response(text.content)
