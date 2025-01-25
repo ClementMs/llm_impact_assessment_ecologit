fa4a16e6d91904b0440d975bcbcab19287dc7be9
ClementMs <clement.msika@gmail.com>
2025-01-25 11:34:35 +0100

    updated

---
+++ lambda_function_test_subscriber.py
@@ -0,0 +1,41 @@
+import json
+
+import pymongo
+from pymongo import MongoClient
+import os
+
+from urllib import parse as urlparse
+import base64
+
+mongodb_connexion_str = os.getenv('mongodb_connexion_string')
+
+mongo_client = MongoClient(mongodb_connexion_str, server_api=pymongo.server_api.ServerApi(
+    version="1", strict=True, deprecation_errors=True))
+
+database = mongo_client["llm_energy_impact"]
+
+collection = database["response"]
+
+def lambda_handler(event, context):
+
+    
+
+    #document_list = [
+   #{ "<field name>" : "<value>" },
+   #{ "<field name>" : "<value>" }
+#]
+
+    msg_map = dict(urlparse.parse_qsl(base64.b64decode(str(event['body'])).decode('ascii')))
+
+    document_list = [{'test': str(event['body']) }]
+
+    result = collection.insert_many(document_list)
+
+    mongo_client.close()
+
+    #print(str(event['body']))
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
